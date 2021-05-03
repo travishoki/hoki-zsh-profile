2a92e3dd810cb1799c282500f420163d23a0fbd0
Travis Hoki <travis.hoki@xant.ai>
2021-05-03 11:55:43 -0600

    nvm

--- nvm.zsh
+++ nvm.zsh
@@ -1,9 +1,3 @@
-. ~/Sites/hoki-zsh-profile/aliases.zsh
-. ~/Sites/hoki-zsh-profile/git.zsh
-. ~/Sites/hoki-zsh-profile/prompt.zsh
-. ~/Sites/hoki-zsh-profile/nvm.zsh
-
-
  export NVM_DIR="$HOME/.nvm"
   [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
   [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
