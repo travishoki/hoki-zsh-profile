7fc1c2d5aa3ad82fea5354c3df30538439260aad
Travis Hoki <travis.hoki@gmail.com>
2021-08-04 05:29:26 -0600

    portfolio

--- .zshrc
+++ .zshrc
@@ -1,5 +1,6 @@
 . ~/Sites/hoki-zsh-profile/aliases.zsh
 . ~/Sites/hoki-zsh-profile/game.zsh
+. ~/Sites/hoki-zsh-profile/portfolio.zsh
 . ~/Sites/hoki-zsh-profile/git.zsh
 . ~/Sites/hoki-zsh-profile/nvm.zsh
 . ~/Sites/hoki-zsh-profile/programs.zsh
---
+++ portfolio.zsh
@@ -0,0 +1,21 @@
+function hokiportfolio() {
+	echo '-- Portfolio --'
+
+	echo 'Got to dir'
+	cd ~/Sites/travishoki-react/
+
+	echo 'Open in VS Code'
+	code .
+
+	echo 'Open in Tower'
+	gittower .
+
+	echo 'Open in Finder'
+	open .
+
+    echo 'Update Node Version'
+	nvm use
+
+	echo 'Launch App in Web'
+	yarn start
+}
